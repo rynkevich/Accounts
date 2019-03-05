70afa5ccfa5ba8a190652af1fa78551b1cbac56d
Arseni Rynkevich <rynkevich@icloud.com>
2019-03-06 01:15:46 +0300

    Add account list view

--- AccountList/ViewControllers/AccountListViewController.swift
+++ AccountList/ViewControllers/AccountListViewController.swift
@@ -2,4 +2,18 @@ import UIKit
 
 class AccountListViewController: UIViewController {
     
+    @IBOutlet weak var accountButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        initAvatarImageView()
+    }
+    
+    func initAvatarImageView() {
+        accountButton.layer.borderColor = UIColor.clear.cgColor
+        accountButton.layer.cornerRadius = accountButton.frame.size.width / 2
+        accountButton.layer.masksToBounds = true
+    }
+    
 }
